9e7a68fdaf433e647ab54e9ef9f3c2173e18b95b
Brian Lai (Sourcetree) <albbrian@gmail.com>
2024-01-11 14:40:11 +0800

    fix the img path

--- src/components/GameModes.tsx
+++ src/components/GameModes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const pathName = process.env.PUBLIC_URL;
+
 function GameModes() {
   const navigate = useNavigate();
 
@@ -15,11 +17,11 @@ function GameModes() {
   return (
     <div className="landing">
       <div className="card" onClick={onClickPvp} onKeyDown={onClickPvp} role="button" tabIndex={0} data-testid="pvp-button">
-        <img src="pvp.png" alt="PVP: player vs player" />
+        <img src={`${pathName}/images/pvp.png`} alt="PVP: player vs player" />
         <h1>Player vs Player</h1>
       </div>
       <div className="card" onClick={onClickPvc} onKeyDown={onClickPvc} role="button" tabIndex={0} data-testid="pvc-button">
-        <img src="pvc.png" alt="PVC: player vs computer" />
+        <img src={`${pathName}/images/pvc.png`} alt="PVC: player vs computer" />
         <h1>Player vs Computer</h1>
       </div>
     </div>
